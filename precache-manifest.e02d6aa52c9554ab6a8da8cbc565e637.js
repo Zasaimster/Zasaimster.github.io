9037a5782e9e212cffc87d402dada32a10132eeb
Zasaimster <Zasaimster@gmail.com>
2020-09-30 20:03:19 -0700

    Updates

---
+++ precache-manifest.e02d6aa52c9554ab6a8da8cbc565e637.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3734ac200bb64309d0763a897dbecbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e18db7e561c890af090f",
+    "url": "/static/js/2.afa29688.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.afa29688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b604b3d185e8f2b1e924",
+    "url": "/static/js/main.f8096419.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  }
+]);
